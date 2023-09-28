500bd50102c677d5ff34a3550c228752801f3189
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2023-09-28 00:12:18 -0300

    will

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,16 @@ export default defineConfig({
 	build: {
 		// optimizaciones del build para que el código final sea generado de mejor manera
 		incremental: true, // acelerar la generación del build
-		rollupOptions: {
-			// me permite configurar el rollup para que no me genere un archivo por cada librería que tenga en el proyecto
-			external: ["react", "react-dom"],
-			output: {
-				globals: {
-					react: "React",
-					"react-dom": "ReactDOM",
-				},
-			},
-		},
+		// rollupOptions: {
+		// 	// me permite configurar el rollup para que no me genere un archivo por cada librería que tenga en el proyecto
+		// 	external: ["react", "react-dom"],
+		// 	output: {
+		// 		globals: {
+		// 			react: "React",
+		// 			"react-dom": "ReactDOM",
+		// 		},
+		// 	},
+		// },
 		// Configuración opcional para TS
 		/*typescript: {
       tsconfig: "./tsconfig.json"
@@ -28,9 +28,9 @@ export default defineConfig({
 		mode: "production", // Especificar un contexto de salida que será para un entorno de producción (opcional)
 		chunks: true, // Habilitar el particionamiento para proveer por trozos el js
 		moduleBundling: true, // me permite tomar el código de las librerías del proyecto para producción y bajarles el tamaño
-	 prerenderPaths: ["/"], // pre-carga en memoria de rutas pesadas
+	//  prerenderPaths: ["/"], // pre-carga en memoria de rutas pesadas
 		// watch: true, // observable de los cambios del código asociado al build
 		modulePreload: true, // me permite a las rutas de pre-render ya tener a mano su código
-		outDir: "build", // me permite cambiarle el nombre al directorio de salida
+	// 	outDir: "build", // me permite cambiarle el nombre al directorio de salida
 	},
 });
